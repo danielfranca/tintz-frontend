d0b278abb6dbad4cbc1752264a8686106d6c1d25
Daniel França <daniel.franca@gmail.com>
2016-10-19 00:07:30 +0200

    Removed meta information on About card

--- app/components/About.js
+++ app/components/About.js
@@ -6,7 +6,6 @@ export default function About(props) {
     return <div>
         <Card>
             <Card.Content header='About Tintz' />
-            <Card.Content meta='Spread your creativity' />
             <Card.Content>
             <p>Lorem ipsum dolor sit amet, ex eos nonumy audiam, eum at affert graecis noluisse. Vitae labores legendos an nec. Et vel liber iuvaret. Corpora scriptorem no vel, eu quas philosophia sed. Soleat option nominati sea at. Quem splendide mnesarchum sit ne, ut movet omittantur eum, pro no viderer gloriatur intellegam.</p>
             <p>Cu cum vero saperet constituam, veri graece cum ex, odio liber placerat eum id. Mei purto dicam impedit et, et moderatius ullamcorper vituperatoribus eos, mea ut populo tincidunt. Quem consulatu est ut, causae eligendi cu pri. Ad offendit dissentias his, melius perfecto id est. </p>
