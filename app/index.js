dc6727f326b1b1abd1da396b8dfaafe9f3b88cbb
Daniel França <daniel.franca@gmail.com>
2016-11-05 00:32:11 +0100

    Added reactotron support

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import routes from './config/routes';
+import Reactotron from 'reactotron-react-js'
+
+Reactotron
+  .configure() // we can use plugins here -- more on this later
+  .connect() // let's connect!
 
 ReactDom.render(
   routes,
